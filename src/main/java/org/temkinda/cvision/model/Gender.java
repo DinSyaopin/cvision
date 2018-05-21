2012937eea5085e60f79b6100e34489b7dec7a92
Dmitry Tiomkin <dmitry.tiomkin@gmail.com>
2018-05-22 01:41:41 +0400

    Add class Gender to model package.

---
+++ src/main/java/org/temkinda/cvision/model/Gender.java
@@ -0,0 +1,36 @@
+package org.temkinda.cvision.model;
+
+public class Gender {
+
+    private int id;
+    private String gender;
+
+    public Gender(int id, String gender) {
+        this.id = id;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Gender{" +
+                "id=" + id +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
